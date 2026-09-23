db471164aa01c32a627e83754eb1c998bf959c5d
ripetchor <115036520+ripetchor@users.noreply.github.com>
2026-09-23 18:07:53 +0500

    feat: solve convertNumberToString

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -123,7 +123,7 @@ function convertToRomanNumerals(/* num */) {
  * Converts a number to a string, replacing digits with words.
  * In this task, the use of methods of the String and Array classes is not allowed.
  *
- * @param {string} numberStr - The number as a string.
+ * @param {string} str - The number as a string.
  * @return {string} The number with digits replaced by words.
  *
  * @example:
@@ -134,8 +134,54 @@ function convertToRomanNumerals(/* num */) {
  *  '10,5'    => 'one zero point five'
  *  '1950.2'  => 'one nine five zero point two'
  */
-function convertNumberToString(/* numberStr */) {
-  throw new Error('Not implemented');
+function convertNumberToString(str) {
+  let result = '';
+
+  for (let i = 0; i < str.length; i += 1) {
+    switch (str[i]) {
+      case '0':
+        result += 'zero';
+        break;
+      case '1':
+        result += 'one';
+        break;
+      case '2':
+        result += 'two';
+        break;
+      case '3':
+        result += 'three';
+        break;
+      case '4':
+        result += 'four';
+        break;
+      case '5':
+        result += 'five';
+        break;
+      case '6':
+        result += 'six';
+        break;
+      case '7':
+        result += 'seven';
+        break;
+      case '8':
+        result += 'eight';
+        break;
+      case '9':
+        result += 'nine';
+        break;
+      case '-':
+        result += 'minus';
+        break;
+      default:
+        result += 'point';
+    }
+
+    if (i < str.length - 1) {
+      result += ' ';
+    }
+  }
+
+  return result;
 }
 
 /**
