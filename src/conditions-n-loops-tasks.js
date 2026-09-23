2a62c2b945b2cca7b24b4c9a0744abc63803f686
ripetchor <115036520+ripetchor@users.noreply.github.com>
2026-09-23 18:10:40 +0500

    feat: solve isPalindrome

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -196,8 +196,14 @@ function convertNumberToString(str) {
  *  '0123210'   => true
  *  'qweqwe'    => false
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  for (let i = 0; i < str.length / 2; i += 1) {
+    if (!is(str[i], str[str.length - 1 - i])) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 /**
